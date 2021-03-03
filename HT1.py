d931908acae9876c67e2fe4149175bfdff108028
marcosItzep <79873129+marcosItzep@users.noreply.github.com>
2021-03-03 16:55:41 -0600

    Add files via upload

--- HT1.py
+++ HT1.py
@@ -1,3 +1,10 @@
-    print("HT 1")
-    X=input("Escribe tu nombre: \n")
-    print(X)
+print("-------------------------------------------")
+print("\n Marcos_Itzep")
+print("HT_1")
+x  =  float ( input ( "Escribe tu altura (m):" ))  #altura
+print("Su altura es: ",x)
+y=float( input("Escribe tu peso (kg):"))   #peso
+print("La altura es: ",y)
+IMC = round ((y/(x*x)) )      #Calculo Ims   
+print("Su masa corporal es (IMC): ",IMC)          #Resultado
+print("-------------------------------------------")
